ee4afc9bb937e8ddd12796b036f461e6baaa40cf
Billy Roh <billy.roh@gmail.com>
2016-10-16 22:52:15 -0700

    draw triangles

--- javascripts/homage.js
+++ javascripts/homage.js
@@ -1,107 +1,108 @@
 // HOMAGE TO THE SQUARE
+// JOSEF ALBERS
 
 // Basic variables
-let maxWidth = 500
-let minWidth = maxWidth * 0.4
+let homage = {
+  maxWidth: 500,
+  dataset: [],
+  innerColor: {h: 55, s: .92, l: .65},
+  outerColor: {h: 79, s: .58, l: .48},
+}
 
 // Palette
-let innerColor = {h: 55, s: .92, l: .65}
-let outerColor = {h: 79, s: .58, l: .48}
-
-let homageDataset = []
-
 function generatePalette(iterationCount) {
-  let hIncrement = (innerColor.h - outerColor.h) / (iterationCount - 1)
-  let sIncrement = (innerColor.s - outerColor.s) / (iterationCount - 1)
-  let lIncrement = (innerColor.l - outerColor.l) / (iterationCount - 1)
+  let hIncrement = (homage.innerColor.h - homage.outerColor.h) / (iterationCount - 1)
+  let sIncrement = (homage.innerColor.s - homage.outerColor.s) / (iterationCount - 1)
+  let lIncrement = (homage.innerColor.l - homage.outerColor.l) / (iterationCount - 1)
 
   for (i of Array(iterationCount).keys()) {
     let color = {
-      h: (outerColor.h + (hIncrement * i)),
-      s: (outerColor.s + (sIncrement * i)),
-      l: (outerColor.l + (lIncrement * i))
+      h: (homage.outerColor.h + (hIncrement * i)),
+      s: (homage.outerColor.s + (sIncrement * i)),
+      l: (homage.outerColor.l + (lIncrement * i))
     }
-    homageDataset.push(color)
+    homage.dataset.push(color)
   }
 }
 
 generatePalette(4)
 
 // d3 setup
-let homageCanvas = d3.select('#homage')
-            .append('svg')
-            .attr('width', maxWidth)
-            .attr('height', maxWidth)
+homage.canvas = d3.select('#homage')
+                  .append('svg')
+                  .attr('width', homage.maxWidth)
+                  .attr('height', homage.maxWidth)
 
-let squares = homageCanvas.selectAll('rect')
-                 .data(homageDataset)
-                 .enter()
-                 .append('rect')
+homage.squares = homage.canvas.selectAll('rect')
+                  .data(homage.dataset)
+                  .enter()
+                  .append('rect')
 
 // Drawing the square
-squares.attr('width', (d, i) => getWidth(i))
-       .attr('height', (d, i) => getWidth(i))
-       .attr('fill', (d) => `hsl(${d.h}, ${d.s * 100}%, ${d.l * 100}%)`)
+homage.squares.attr('width', (d, i) => getWidth(i))
+  .attr('height', (d, i) => getWidth(i))
+  .attr('fill', (d) => `hsl(${d.h}, ${d.s * 100}%, ${d.l * 100}%)`)
 
 function getWidth(index) {
-  let increment = (maxWidth - minWidth) / (homageDataset.length - 1)
-  return maxWidth - (increment * index)
+  let minWidth = homage.maxWidth * 0.4
+  let increment = (homage.maxWidth - minWidth) / (homage.dataset.length - 1)
+  return homage.maxWidth - (increment * index)
 }
 
 setCoordinates()
 
 function setCoordinates() {
-  squares.attr('transform', (d, i) => {
+  homage.squares.attr('transform', (d, i) => {
     return `translate(${getInitialX(i)}, ${getInitialY(i)}) rotate(0)`
   })
 }
 
 function getInitialX(index) {
-  return (maxWidth - getWidth(index)) / 2
+  return (homage.maxWidth - getWidth(index)) / 2
 }
 
 function getInitialY(index) {
-  let increment = maxWidth * 0.05
-  return maxWidth - getWidth(index) - (increment * index)
+  let increment = homage.maxWidth * 0.05
+  return homage.maxWidth - getWidth(index) - (increment * index)
 }
 
 // Interaction
-homageCanvas.on('mousemove', function () {
+homage.canvas.on('mousemove', function () {
   let xCoordinate = d3.mouse(this)[0]
   let yCoordinate = d3.mouse(this)[1]
 
   // Use a transition for the initial animation
-  if (!initialAnimationDidFinish) {
-    homageCanvas.selectAll('rect')
+  if (!homage.initialAnimationDidFinish) {
+    homage.canvas.selectAll('rect')
       .interrupt()
-    homageCanvas.selectAll('rect')
       .transition(d3.easeBounce)
         .duration(75)
         .attr('transform', (d, i) => getNewTransform(i, xCoordinate, yCoordinate))
-        .on('end', () => { initialAnimationDidFinish = true})
+        .on('end', () => { homage.initialAnimationDidFinish = true})
   // Once the initial animation is finished, track the cursor linearly
   } else {
-    squares.attr('transform', (d, i) => getNewTransform(i, xCoordinate, yCoordinate))
+    homage.squares.attr('transform', (d, i) => getNewTransform(i, xCoordinate, yCoordinate))
   }
 
   // TODO (bonus point) add drop shadow when animating
+  // TODO (bonus point) click to change colours and have the squares radiate out
 })
 
-homageCanvas.on('mouseleave', function () {
-  homageCanvas.selectAll('rect')
+homage.canvas.on('mouseleave', function () {
+  homage.canvas.selectAll('rect')
     .transition(d3.easeElasticInOut)
       .duration(250)
       .attr('transform', (d, i) => {
         if (i === 0) { return }
         return `translate(${getInitialX(i)}, ${getInitialY(i)}) rotate(0)`
       })
-      .on('end', () => { initialAnimationDidFinish = false})
+      .on('end', () => { homage.initialAnimationDidFinish = false})
 })
 
-let initialAnimationDidFinish = false
+homage.initialAnimationDidFinish = false
 
-let cursorScale = d3.scaleLinear()
-  .domain([0, maxWidth])
+homage.cursorScale = d3.scaleLinear()
+  .domain([0, homage.maxWidth])
   .range([-1, 1])
   .clamp(true)
 
@@ -113,16 +114,16 @@ function getNewTransform(i, xCoordinate, yCoordinate) {
 }
 
 function getNewRotation(i, xCoordinate) {
-  let rIncrement = (i / homageDataset.length) * 10
-  return cursorScale(xCoordinate) * rIncrement
+  let rIncrement = (i / homage.dataset.length) * 10
+  return homage.cursorScale(xCoordinate) * rIncrement
 }
 
 function getNewX(i, xCoordinate) {
-  let xIncrement = (i / homageDataset.length) * (maxWidth * 0.2)
-  return getInitialX(i) + (cursorScale(xCoordinate) * xIncrement)
+  let xIncrement = (i / homage.dataset.length) * (homage.maxWidth * 0.2)
+  return getInitialX(i) + (homage.cursorScale(xCoordinate) * xIncrement)
 }
 
 function getNewY(i, yCoordinate) {
-  let yIncrement = (i / homageDataset.length) * (maxWidth * 0.2)
-  return getInitialY(i) + (cursorScale(yCoordinate) * yIncrement)
+  let yIncrement = (i / homage.dataset.length) * (homage.maxWidth * 0.2)
+  return getInitialY(i) + (homage.cursorScale(yCoordinate) * yIncrement)
 }
