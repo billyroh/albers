986089d9bf3b5f4f97cf3c14ab382030b8a08785
Billy Roh <billy.roh@gmail.com>
2017-05-21 20:38:05 -0700

    add ruby version, add upward

---
+++ javascripts/upward.js
@@ -0,0 +1,153 @@
+// UPWARD
+// JOSEF & ANNI ALBERS
+
+// Basic variables
+var upward = {
+  width: d3.select('#upward').node().getBoundingClientRect().width,
+}
+
+// Grab height of the canvas
+// Divide it by 40 to get the width and height of the 'pixel'
+let pixelValue = _.round(upward.width / 40);
+
+upward.palette = getUpwardPalette()
+
+function getUpwardPalette() {
+  return _.sample(['black', 'grey', 'white'])
+}
+
+
+// d3 setup
+upward.canvas =
+  d3.select('#upward')
+      .append('svg')
+      .attr('width', fox.width)
+      .attr('height', fox.width)
+
+upward.canvas.style('background-color', 'grey')
+
+// Horizontal bars
+function generateHorizontalBars() {
+  let width = _.random(4, 40)
+  let numberOfBars = _.random(3, 10)
+
+  let xOffset = _.random(30) * pixelValue
+  let yOffset = _.random(30) * pixelValue
+
+  let path = ''
+  for (let i = 0; i < numberOfBars; i++) {
+    let baseY = i * pixelValue * 2
+    let point1 = `M ${xOffset}, ${yOffset + baseY}`
+    let point2 = `L ${xOffset + width * pixelValue}, ${yOffset + baseY}`
+    let point3 = `L ${xOffset + width * pixelValue}, ${yOffset + baseY + pixelValue}`
+    let point4 = `L ${xOffset}, ${yOffset + baseY + pixelValue} Z`
+    path += `${point1} ${point2} ${point3} ${point4}`
+  }
+
+  return {
+    type: 'horizontal',
+    path: path,
+    color: getUpwardPalette(),
+  }
+}
+
+let horizontalBarsData = []
+let numberOfHorizontalBars = _.random(10, 30)
+for (let i = 0; i < numberOfHorizontalBars; i++) {
+  horizontalBarsData.push(generateHorizontalBars())
+}
+
+// upward.horizontalBars = upward.canvas.selectAll('path.horizontal')
+//   .data(horizontalBarsData)
+//   .enter()
+//   .append('path')
+//     .attr('class', (d) => d.type)
+//     .attr('d', (d) => d.path)
+//     .attr('fill', (d) => d.color)
+
+// Vertical bars
+function generateVerticalBars() {
+  let height = _.random(4, 10)
+  let numberOfBars = _.random(3, 8)
+
+  let xOffset = _.random(30) * pixelValue
+  let yOffset = _.random(30) * pixelValue
+
+  let path = ''
+  for (let i = 0; i < numberOfBars; i++) {
+    let baseX = i * pixelValue * 2
+
+    let point1 = `M ${xOffset + baseX}, ${yOffset}`
+    let point2 = `L ${xOffset + baseX + pixelValue}, ${yOffset}`
+    let point3 = `L ${xOffset + baseX + pixelValue}, ${yOffset + height * pixelValue}`
+    let point4 = `L ${xOffset + baseX}, ${yOffset + height * pixelValue} Z`
+    path += `${point1} ${point2} ${point3} ${point4}`
+  }
+
+  return {
+    type: 'vertical',
+    path: path,
+    color: getUpwardPalette(),
+  }
+}
+
+let verticalBarsData = []
+let numberOfVerticalBars = _.random(5, 10)
+for (let i = 0; i < numberOfVerticalBars; i++) {
+  verticalBarsData.push(generateVerticalBars())
+}
+
+// upward.verticalBars = upward.canvas.selectAll('path.vertical')
+//   .data(verticalBarsData)
+//   .enter()
+//   .append('path')
+//     .attr('class', (d) => d.type)
+//     .attr('d', (d) => d.path)
+//     .attr('fill', (d) => d.color)
+
+
+// Rectangles
+function generateRectangles() {
+  let xOffset = _.random(30) * pixelValue
+  let yOffset = _.random(30) * pixelValue
+
+  let width = _.random(5, 20) * pixelValue
+  let height = _.random(5, 20) * pixelValue
+
+  let point1 = `M ${xOffset}, ${yOffset}`
+  let point2 = `L ${xOffset + width}, ${yOffset}`
+  let point3 = `L ${xOffset + width}, ${yOffset + height}`
+  let point4 = `L ${xOffset}, ${yOffset + height} Z`
+
+  return {
+    type: 'rectangle',
+    path: `${point1} ${point2} ${point3} ${point4}`,
+    color: getUpwardPalette(),
+  }
+}
+
+let rectangleData = []
+let numberOfRectangles = _.random(5, 10)
+for (let i = 0; i < numberOfRectangles; i++) {
+  rectangleData.push(generateRectangles())
+}
+
+let dataset = _.flatten([horizontalBarsData, verticalBarsData])
+dataset = _.shuffle(dataset)
+dataset = _.flatten([rectangleData, dataset])
+
+// upward.rectangles = upward.canvas.selectAll('path.rectangle')
+//   .data(rectangleData)
+//   .enter()
+//   .append('path')
+//     .attr('class', (d) => { console.log('d', d); return d.type })
+//     .attr('d', (d) => d.path)
+//     .attr('fill', (d) => d.color)
+
+upward.canvas.selectAll('path')
+  .data(dataset)
+  .enter()
+  .append('path')
+    .attr('class', (d) => d.type)
+    .attr('d', (d) => d.path)
+    .attr('fill', (d) => d.color)
