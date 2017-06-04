e18d1f4112c3de9d2761a8334e90ac5d0cae7cdb
Billy Roh <billy.roh@gmail.com>
2017-06-03 17:32:10 -0700

    got the rows to render correctly

--- javascripts/frontal.js
+++ javascripts/frontal.js
@@ -16,53 +16,166 @@ frontal.canvas = d3.select('#frontal')
 
 // I'm using this naming scheme because narrow/wide, short/tall gets confusing.
 let angelHair = _.round(frontal.width / 200)
-let penne = angelHair *  4
+let penne = angelHair * 3
 
-function generateDarkPenne() {
+// Helper functions for getting the x position of each of the elements.
+function getPennePosition(index) {
+  // index is out of 17
+  return index * angelHair * 10
+}
+
+function getAngelHairPosition(index) {
+  // index is out of 48
+  let pennePosition = getPennePosition(_.floor(index / 3))
+  let penneElement = pennePosition + penne
+  let subIndex = index % 3
+  return penneElement + angelHair + (subIndex * angelHair * 2)
+}
+
+// Helper functions for creating the data for each of the elements.
+function generateDarkPenne(startX) {
   let result = []
-  let minWidth = _.round(frontal.width * 0.1)
-  let maxWidth = _.round(frontal.width * 0.2)
-  let width = _.random(minWidth, maxWidth)
-  let numberOfRows = _.random(6, 8)
-  let xPosition = _.random(minWidth, maxWidth)
-  let yPosition = _.random(minWidth, maxWidth)
 
-  for (let i = 0; i < numberOfRows; i++) {
+  let minWidth = _.round(frontal.width * 0.075)
+  let maxWidth = _.round(frontal.width * 0.1)
+  let width = _.random(minWidth, maxWidth)
+
+  let minX = startX
+  let maxX = startX + _.round(frontal.width * 0.3)
+  let xPosition = _.random(minX, maxX)
+
+  let minY = _.random(1, 8)
+  let numberOfRows = _.random(6, 8)
+
+  for (let i = minY; i < minY + numberOfRows; i++) {
     let row = {
       x: xPosition,
-      y: yPosition + (i * angelHair * 10),
+      y: getPennePosition(i),
       width: width,
     }
     result.push(row)
   }
+
   return result
 }
 
-function generateDarkAngelHair() {
+function generateDarkAngelHair(startX) {
   let result = []
+
   let minWidth = _.round(frontal.width * 0.5)
   let maxWidth = _.round(frontal.width * 0.8)
   let width = _.random(minWidth, maxWidth)
-  let numberOfRows = _.random(4, 8)
-  let xPosition = _.random(minWidth, maxWidth)
-  let yPosition = _.random(minWidth, maxWidth)
 
-  for (let i = 0; i < numberOfRows; i++) {
+  let minX = startX
+  let maxX = startX + _.round(frontal.width * 0.3)
+  let xPosition = _.random(minX, maxX)
+
+  let minY = _.random(1, 8)
+  let numberOfRows = _.random(4, 8)
+
+  for (let i = minY; i < minY + numberOfRows; i++) {
+    let index = getDarkAngelHairIndex(i)
     let row = {
       x: xPosition,
-      y: yPosition + (i * angelHair * 13),
+      y: getAngelHairPosition(index),
       width: width,
     }
     result.push(row)
   }
+
   return result
 }
 
+function getDarkAngelHairIndex(index) {
+  return 1 + ((index - 1) * 3)
+}
 
-let darkAngelHairData = generateDarkAngelHair()
-let lightAngelHairData = generateDarkAngelHair()
-let darkPenneData = generateDarkPenne()
+function generateLightAngelHair(startX) {
+  let result = []
 
+  let minWidth = _.round(frontal.width * 0.5)
+  let maxWidth = _.round(frontal.width * 0.8)
+  let width = _.random(minWidth, maxWidth)
+
+  let minX = startX
+  let maxX = startX + _.round(frontal.width * 0.3)
+  let xPosition = _.random(minX, maxX)
+
+  let minY = _.random(1, 8)
+  let numberOfRows = _.random(4, 8)
+
+  for (let i = minY; i < minY + numberOfRows; i++) {
+    let index = getLightAngelHairIndex(i)
+    let row = {
+      x: xPosition,
+      y: getAngelHairPosition(index),
+      width: width,
+    }
+    result.push(row)
+  }
+
+  return result
+}
+
+function getLightAngelHairIndex(index) {
+  // if 0
+  if (index === 0) {
+    return 0
+  // if even
+  } else if (index % 2 === 0) {
+    return 2 + (3 * (index - 1))
+  // if odd
+  } else if (index % 2 === 1) {
+    return 3 + (3 * (index - 1))
+  }
+}
+
+// Dark penne
+let numberOfPenneColumns = _.random(4, 6)
+let darkPenneData = []
+let previousX = 0
+
+for (let i = 0; i < numberOfPenneColumns; i++) {
+  let minX = _.round(frontal.width * 0.05)
+  let maxX = _.round(frontal.width * 0.2)
+  let startX = previousX + _.random(minX, maxX)
+  darkPenneData.push(generateDarkPenne(startX))
+  previousX = startX
+}
+
+darkPenneData = _.flatten(darkPenneData)
+
+// Dark angel hair
+let numberOfDarkAngelHairColumns = _.random(4, 6)
+let darkAngelHairData = []
+previousX = 0
+
+for (let i = 0; i < numberOfDarkAngelHairColumns; i++) {
+  let minX = _.round(frontal.width * 0.05)
+  let maxX = _.round(frontal.width * 0.2)
+  let startX = previousX + _.random(minX, maxX)
+  darkAngelHairData.push(generateDarkAngelHair(startX))
+  previousX = startX
+}
+
+darkAngelHairData = _.flatten(darkAngelHairData)
+
+// Light angel hair
+let numberOfLightAngelHairColumns = _.random(4, 6)
+let lightAngelHairData = []
+previousX = 0
+
+for (let i = 0; i < numberOfLightAngelHairColumns; i++) {
+  let minX = _.round(frontal.width * 0.05)
+  let maxX = _.round(frontal.width * 0.2)
+  let startX = previousX + _.random(minX, maxX)
+  lightAngelHairData.push(generateLightAngelHair(startX))
+  previousX = startX
+}
+
+lightAngelHairData = _.flatten(lightAngelHairData)
+
+// Draw
 frontal.darkAngelHair = frontal.canvas.selectAll('rect.dark-angel-hair')
                   .data(darkAngelHairData)
                   .enter()
