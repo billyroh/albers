4aaa2eadc599dfd64c995147598fac817b824d28
Billy Roh <billy.roh@gmail.com>
2017-06-05 21:28:44 -0700

    orchestra kind of works

--- javascripts/orchestra.js
+++ javascripts/orchestra.js
@@ -77,24 +77,88 @@ orchestra.backgroundLines.attr('d', backgroundLineDataset)
   .attr('class', 'background-line')
 
 // Interaction
+foregroundRotate = 10
+backgroundRotate = foregroundRotate * .5
+foregroundTranslate = orchestra.width * .2
+backgroundTranslate = foregroundTranslate * .2
+
 orchestra.cursorScale = d3.scaleLinear()
   .domain([0, fox.width])
   .range([-1, 1])
   .clamp(true)
 
-orchestra.rotateXScale = d3.scaleLinear()
+orchestra.foregroundRotateScale = d3.scaleLinear()
   .domain([-1, 1])
-  .range([-30, 30])
+  .range([(foregroundRotate * -1), foregroundRotate])
   .clamp(true)
+
+orchestra.foregroundTranslateScale = d3.scaleLinear()
+  .domain([-1, 1])
+  .range([(foregroundTranslate * -1), foregroundTranslate])
+  .clamp(true)
+
+orchestra.backgroundRotateScale = d3.scaleLinear()
+  .domain([-1, 1])
+  .range([(backgroundRotate * -1), backgroundRotate])
+  .clamp(true)
+
+orchestra.backgroundTranslateScale = d3.scaleLinear()
+  .domain([-1, 1])
+  .range([(backgroundTranslate * -1), backgroundTranslate])
+  .clamp(true)
+
+orchestra.scaleScale = d3.scaleLinear()
+  .domain([0, 0.7])
+  .range([1, 0.98])
+  .clamp(true)
+
+orchestra.opacityScale = d3.scaleLinear()
+  .domain([0, 0.75])
+  .range([0, 0.3])
+  .clamp(true)
+
+function foregroundTransformString(xCoordinate, yCoordinate) {
+  let scaledX = orchestra.cursorScale(xCoordinate)
+  let scaledY = orchestra.cursorScale(yCoordinate)
+  let rotateXString = `rotateY(${orchestra.foregroundRotateScale(scaledX)}deg)`
+  let rotateYString = `rotateX(${orchestra.foregroundRotateScale(scaledY) * -1}deg)`
+  let rotateZString = `rotateZ(0)`
+  let rotateString = `${rotateXString} ${rotateYString} ${rotateZString}`
+  let translateXString = `translateX(${orchestra.foregroundRotateScale(scaledX)}px)`
+  let translateYString = `translateY(${orchestra.foregroundRotateScale(scaledY)}px)`
+  // return `${rotateXString} ${rotateYString} ${translateXString} ${translateYString}`
+  return `perspective(${orchestra.width * 3}px) ${rotateString} ${translateXString} ${translateYString}`
+}
+
+function backgroundTransformString(xCoordinate, yCoordinate) {
+  let scaledX = orchestra.cursorScale(xCoordinate)
+  let scaledY = orchestra.cursorScale(yCoordinate)
+  let rotateXString = `rotateY(${orchestra.backgroundRotateScale(scaledX)}deg)`
+  let rotateYString = `rotateX(${orchestra.foregroundRotateScale(scaledY) * -1}deg)`
+  let rotateZString = `rotateZ(0)`
+  let rotateString = `${rotateXString} ${rotateYString} ${rotateZString}`
+  let scaleString = `scale(${orchestra.scaleScale(Math.abs(scaledX) + Math.abs(scaledY))})`
+  let translateXString = `translateX(${orchestra.foregroundRotateScale(scaledX)}px)`
+  let translateYString = `translateY(${orchestra.foregroundRotateScale(scaledY)}px)`
+  // return `${rotateXString} ${rotateYString} ${scaleString} ${translateXString} ${translateYString}`
+  return `perspective(${orchestra.width * 3}px) ${rotateString} ${scaleString} ${translateXString} ${translateYString}`
+}
+
+function backgroundOpacity(xCoordinate, yCoordinate) {
+  let scaledX = orchestra.cursorScale(xCoordinate)
+  let scaledY = orchestra.cursorScale(yCoordinate)
+  return 1 - orchestra.opacityScale(Math.abs(scaledX) + Math.abs(scaledY))
+}
 
 orchestra.canvas.on('mousemove', function () {
   let xCoordinate = d3.mouse(this)[0]
   let yCoordinate = d3.mouse(this)[1]
-  let scaledX = orchestra.cursorScale(xCoordinate)
-  let scaledY = orchestra.cursorScale(yCoordinate)
 
   orchestra.foregroundLines
-    .style('transform', `rotateY(${orchestra.rotateXScale(scaledX)}deg)`)
+    .style('transform', foregroundTransformString(xCoordinate, yCoordinate))
+    .style('transform-origin', '50% 50% 0')
   orchestra.backgroundLines
-    .style('transform', `rotateY(-${orchestra.rotateXScale(scaledX)}deg)`)
+    .style('transform', backgroundTransformString(xCoordinate, yCoordinate))
+    .style('opacity', backgroundOpacity(xCoordinate, yCoordinate))
+    .style('transform-origin', '50% 50% 0')
 })
